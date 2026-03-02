b3d115e103a76d32188c7e845a3ac799de41a654
Zimamazim (Martin Zimen) <zimenm@seznam.cz>
2026-03-02 11:43:36 +0100

    Include scoreError in the dataframe

--- processing/src/main/kotlin/JmhJson.kt
+++ processing/src/main/kotlin/JmhJson.kt
@@ -20,12 +20,13 @@ fun extractMetric(it: JsonElement, metric: String = ""): List<List<Double>> = it
     }
     .toList()
 
-fun loadScores(json: JsonElement): Map<String, Double> = json
+fun loadScores(json: JsonElement): Map<String, Pair<Double, Double>> = json
     .jsonArray.map {
         val name = it.jsonObject["benchmark"]!!.jsonPrimitive.content.substringAfterLast(".")
         val score = it.jsonObject["primaryMetric"]!!.jsonObject["score"]!!.jsonPrimitive.double
+        val error = it.jsonObject["primaryMetric"]!!.jsonObject["scoreError"]!!.jsonPrimitive.double
         val params = it.jsonObject["params"]?.jsonObject?.toMap()?.mapValues { it.value.jsonPrimitive.toString() }
                         ?.takeIf { it.isNotEmpty() }?.toString()?.replace(" ", "") ?: ""
-        (name + params) to score
+        (name + params) to (score to error)
     }
     .toMap()
